38055ec3793c3a2ba644f55f29de98a36da508ee
Sreeram A <sreeramraghu04@gmail.com>
2025-10-26 03:47:14 +0530

    Update AdminCarPricing.jsx

--- src/pages/adminpages/adminnested/AdminCarPricing.jsx
+++ src/pages/adminpages/adminnested/AdminCarPricing.jsx
@@ -45,7 +45,7 @@ const AdminCarPricing = () => {
           <li>Restoration Cost: {car.restoration}</li>
           <li>Tax & Registration: {car.registration}</li>
           <li className="text-green-400 font-semibold">
-            Total Value: ₹{Number(car.price) + (car.restoration) + (car.restoration)}
+Total Value: ₹{Number(car.price || 0) + Number(car.restoration || 0) + Number(car.registration || 0)}
           </li>
         </ul>
       </div>
